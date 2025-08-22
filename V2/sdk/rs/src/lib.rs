49ddbba50b41e047f7ac7ffa98e29b2225bad660
Venom 🎱 <155748950+88qji@users.noreply.github.com>
2025-08-22 14:51:53 -0600

    Update lib.rs

--- V2/sdk/rs/src/lib.rs
+++ V2/sdk/rs/src/lib.rs
@@ -1,1 +1,26 @@
+// Re-export dependencies for convenience
+pub use solana_client;
 
+pub use squads_multisig_program;
+pub use squads_multisig_program::anchor_lang;
+pub use squads_multisig_program::anchor_lang::solana_program;
+
+pub mod client;
+pub mod pda;
+pub mod vault_transaction;
+
+pub mod error {
+    use thiserror::Error;
+
+    #[derive(Debug, Error)]
+    pub enum ClientError {
+        #[error(transparent)]
+        Client(#[from] solana_client::client_error::ClientError),
+        #[error("Failed to deserialize account data")]
+        DeserializationError,
+        #[error("Invalid AddressLookupTableAccount")]
+        InvalidAddressLookupTableAccount,
+        #[error("Invalid TransactionMessage")]
+        InvalidTransactionMessage,
+    }
+}
