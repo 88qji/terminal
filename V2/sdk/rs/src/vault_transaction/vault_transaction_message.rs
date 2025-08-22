f9a6e4d26a0deff3de47b7f0839b17756d91e223
Venom 🎱 <155748950+88qji@users.noreply.github.com>
2025-08-22 14:54:49 -0600

    Update vault_transaction_message.rs

--- V2/sdk/rs/src/vault_transaction/vault_transaction_message.rs
+++ V2/sdk/rs/src/vault_transaction/vault_transaction_message.rs
@@ -1,1 +1,19 @@
+use crate::pda::get_ephemeral_signer_pda;
+use squads_multisig_program::{
+    CompiledInstruction, MessageAddressTableLookup, TransactionMessage, VaultTransactionMessage,
+};
+use std::collections::HashMap;
 
+use super::compiled_keys::CompiledKeys;
+use crate::solana_program::address_lookup_table_account::AddressLookupTableAccount;
+use crate::solana_program::instruction::{AccountMeta, Instruction};
+use crate::solana_program::message::{AccountKeys, CompileError};
+use crate::solana_program::pubkey::Pubkey;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Invalid AddressLookupTableAccount")]
+    InvalidAddressLookupTableAccount,
+    #[error("Invalid TransactionMessage")]
+    InvalidTransactionMessage,
+}
