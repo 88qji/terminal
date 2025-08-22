26ddd0db60c69be253132ee05686a407a2b3c4fa
Venom 🎱 <155748950+88qji@users.noreply.github.com>
2025-08-22 14:52:36 -0600

    Update pda.rs

--- V2/sdk/rs/src/pda.rs
+++ V2/sdk/rs/src/pda.rs
@@ -19,3 +19,51 @@ pub fn get_multisig_pda(create_key: &Pubkey, program_id: Option<&Pubkey>) -> (Pu
     )
 }
 
+pub fn get_vault_pda(
+    multisig_pda: &Pubkey,
+    index: u8,
+    program_id: Option<&Pubkey>,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[
+            SEED_PREFIX,
+            multisig_pda.to_bytes().as_ref(),
+            SEED_VAULT,
+            &[index],
+        ],
+        program_id.unwrap_or(&squads_multisig_program::ID),
+    )
+}
+
+pub fn get_transaction_pda(
+    multisig_pda: &Pubkey,
+    transaction_index: u64,
+    program_id: Option<&Pubkey>,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[
+            SEED_PREFIX,
+            multisig_pda.to_bytes().as_ref(),
+            SEED_TRANSACTION,
+            transaction_index.to_le_bytes().as_ref(),
+        ],
+        program_id.unwrap_or(&squads_multisig_program::ID),
+    )
+}
+
+pub fn get_proposal_pda(
+    multisig_pda: &Pubkey,
+    transaction_index: u64,
+    program_id: Option<&Pubkey>,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[
+            SEED_PREFIX,
+            multisig_pda.to_bytes().as_ref(),
+            SEED_TRANSACTION,
+            &transaction_index.to_le_bytes(),
+            SEED_PROPOSAL,
+        ],
+        program_id.unwrap_or(&squads_multisig_program::ID),
+    )
+}
