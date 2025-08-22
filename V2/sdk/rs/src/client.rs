e2f8db19788c4cd6b041cd93ce4d55f8d3cae861
Venom 🎱 <155748950+88qji@users.noreply.github.com>
2025-08-22 14:49:51 -0600

    Update client.rs

--- V2/sdk/rs/src/client.rs
+++ V2/sdk/rs/src/client.rs
@@ -56,3 +56,68 @@ pub async fn get_multisig(rpc_client: &RpcClient, multisig_key: &Pubkey) -> Clie
 
     Ok(multisig)
 }
+
+/// Gets a `SpendingLimit` account from the chain.
+pub async fn get_spending_limit(
+    rpc_client: &RpcClient,
+    spending_limit_key: &Pubkey,
+) -> ClientResult<SpendingLimit> {
+    let spending_limit_account = rpc_client.get_account(spending_limit_key).await?;
+
+    let spending_limit =
+        SpendingLimit::try_deserialize(&mut spending_limit_account.data.as_slice())
+            .map_err(|_| ClientError::DeserializationError)?;
+
+    Ok(spending_limit)
+}
+
+/// Creates a new multisig config transaction.
+/// Example:
+/// ```
+/// use squads_multisig::anchor_lang::error::ComparedValues::Pubkeys;
+/// use squads_multisig::solana_program::pubkey::Pubkey;
+/// use squads_multisig::solana_program::system_program;
+/// use squads_multisig::state::{ConfigAction, Member, Permissions, Permission};
+/// use squads_multisig::client::{
+///     MultisigCreateAccountsV2,
+///     MultisigCreateArgsV2,
+///     multisig_create_v2
+/// };
+///
+/// let ix = multisig_create_v2(
+///     MultisigCreateAccountsV2 {
+///         program_config: Pubkey::new_unique(),
+///         treasury: Pubkey::new_unique(),
+///         multisig: Pubkey::new_unique(),
+///         create_key: Pubkey::new_unique(),
+///         creator: Pubkey::new_unique(),
+///         system_program: system_program::id(),
+///     },
+///     MultisigCreateArgsV2 {
+///         members: vec![
+///             Member {
+///                 key: Pubkey::new_unique(),
+///                 permissions: Permissions::from_vec(&[Permission::Initiate, Permission::Vote, Permission::Execute]),
+///             }
+///         ],
+///         threshold: 1,
+///         time_lock: 0,
+///         config_authority: None,
+///         rent_collector: None,
+///         memo: Some("Deploy my own Squad".to_string()),
+///     },
+///     Some(squads_multisig_program::ID)
+/// );
+/// ```
+///
+pub fn multisig_create_v2(
+    accounts: MultisigCreateAccountsV2,
+    args: MultisigCreateArgsV2,
+    program_id: Option<Pubkey>,
+) -> Instruction {
+    Instruction {
+        accounts: accounts.to_account_metas(Some(false)),
+        data: MultisigCreateDataV2 { args }.data(),
+        program_id: program_id.unwrap_or(squads_multisig_program::ID),
+    }
+}
